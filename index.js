58fb40f208629a4fe5c91236fd02d3f907bf79a4
Jay-Ubisse <joaquimubisse@gmail.com>
2023-05-04 20:18:18 +0200

    configure handlebars view engine

--- index.js
+++ index.js
@@ -1,30 +1,49 @@
 const express = require('express')
+const expressHandlebars = require('express-handlebars')
+
 const app = express()
+
+//configure handlebars view engine
+app.engine('handlebars', expressHandlebars.engine({
+    defaultLayout: 'main',
+}))
+app.set('view engine', 'handlebars')
+
+app.use(express.static(__dirname + '/public'))
+
+//define listen port number
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-    res.type('text/plain')
-    res.send("Home page")
-})
+const fortunes = [
+    "Conquer your fears or thet will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple.",
+]
+
+//get routes
+app.get('/', (req, res) => res.render('home'))
 
 app.get('/about', (req, res) => {
-    res.type('text/plain')
-    res.send("About page")
+    const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
+    res.render('about', {fortune: randomFortune})
 })
 
 // custom 404 page
 app.use((req, res) => {
 res.type('text/plain')
 res.status(404)
-res.send('404 - Not Found')
+res.render('404')
 })
 // custom 500 page
 app.use((err, req, res, next) => {
 console.error(err.message)
 res.type('text/plain')
 res.status(500)
-res.send('500 - Server Error')
+res.render('500')
 })
+
 app.listen(port, () => console.log(
 `Express started on http://localhost:${port}; ` +
 `press Ctrl-C to terminate.`))
